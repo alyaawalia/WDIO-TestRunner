ea0d0dedc4179827205b37da3f42967e7cfa19a3
alyaawalia <alyawalia17@gmail.com>
2024-06-03 23:13:44 +0700

    method

---
+++ test/specs/belajar-method.spec.js
@@ -0,0 +1,35 @@
+import { browser, $, expect } from '@wdio/globals'
+
+describe('Belajar Method', function (){
+    it('Method click', async function (){
+        await browser.url('https://webdriver.io/docs/api/element/click')
+
+        const themeToggleButton = await $ ('.toggleButton_gllP')
+        await themeToggleButton.click()
+
+        await browser.pause(4000)
+
+    })
+
+    it('Method getText', async function (){
+        await browser.url('https://webdriver.io/docs/api/element/getText')
+
+        const paragraf = await $ ('.theme-doc-markdown p')
+        const paragrafText = await paragraf.getText()
+
+        console.log('<||||> ini text nya <||||>', paragrafText)
+    })
+
+    it.only('Method setValue & getValue', async function (){
+        await browser.url('https://duckduckgo.com/')
+
+        const searchInput = await $ ('#searchbox_input')
+        await searchInput.setValue('Contoh pencarian')
+
+        const searchInputValue = await searchInput.getValue()
+
+        await browser.pause(3000)
+
+        console.log('<||||> ini text nya <||||>', searchInputValue)
+    })
+})
